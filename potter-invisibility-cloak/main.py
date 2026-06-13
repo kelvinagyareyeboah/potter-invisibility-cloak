8e76d6753d1635ce3f61f171022140b4b753f7d5
Kelvin Agyare Yeboah <kelvinagyareyeboah@gmail.com>
2026-06-13 00:42:36 +0000

    Update main.py

--- potter-invisibility-cloak/main.py
+++ potter-invisibility-cloak/main.py
@@ -5,8 +5,7 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
-    print("Error: Could no
-    exit()
+    print(
 
 # Set camera resolution
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
